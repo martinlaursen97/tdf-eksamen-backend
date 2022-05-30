36b97dd33ef7d4444ce01c6be2304a0cfa8f5b3b
martinlaursen97 <martin_laursen21@hotmail.com>
2022-05-30 10:18:12 +0200

    InitData class

---
+++ src/main/java/com/code/tdfeksamenbackend/config/InitData.java
@@ -0,0 +1,13 @@
+package com.code.tdfeksamenbackend.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitData implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
