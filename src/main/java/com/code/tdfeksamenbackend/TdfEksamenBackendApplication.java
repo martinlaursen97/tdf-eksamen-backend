14b56aaf1fb75a49c4f8074a561c66b672bc57ec
martinlaursen97 <martin_laursen21@hotmail.com>
2022-05-30 10:13:54 +0200

    first commit

---
+++ src/main/java/com/code/tdfeksamenbackend/TdfEksamenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.code.tdfeksamenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdfEksamenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdfEksamenBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code/tdfeksamenbackend/TdfEksamenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.tdfeksamenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdfEksamenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
